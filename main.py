1a9e8c192aa25d06074a9b25aaf86d0572d544f8
Valeogamer <zhilin.valentin.72@gamil.com>
2023-04-20 22:27:38 +0300

    n commit

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Я тест!")
+import numpy as np
+import random
+rnd = np.array([random.random() for i in range(10)])
+print(rnd)
+with open("rnd_num.txt", "w") as file:
+    file.write(str(rnd))
